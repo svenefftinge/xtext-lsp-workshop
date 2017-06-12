cc32c195e07a01dc6824b7ecd5eb6644a12eefb8
Sven Efftinge <sven.efftinge@typefox.io>
2017-06-12 16:38:50 +0200

    [LSP4E] local reference to jar

--- org.xtext.calc.lsp4e/src/org/xtext/calc/lsp4e/CalculatorLanguageServer.java
+++ org.xtext.calc.lsp4e/src/org/xtext/calc/lsp4e/CalculatorLanguageServer.java
@@ -1,10 +1,16 @@
 package org.xtext.calc.lsp4e;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.osgi.framework.Bundle;
 
 public class CalculatorLanguageServer extends ProcessStreamConnectionProvider {
 
@@ -12,7 +18,13 @@ public class CalculatorLanguageServer extends ProcessStreamConnectionProvider {
 		List<String> commands = new ArrayList<>();
 		commands.add("java");
 		commands.add("-jar");
-		commands.add("/Users/efftinge/Documents/Eclipse/xtext-lsp-workshop/org.xtext.calc.parent/org.xtext.calc.ide/build/libs/calculator-language-server.jar");
+		Bundle bundle = Activator.getDefault().getBundle();
+		URL resource = bundle.getResource("/language-server/calculator-language-server.jar");
+		try {
+			commands.add(new File(FileLocator.resolve(resource).toURI()).getAbsolutePath());
+		} catch (URISyntaxException | IOException e) {
+			throw new IllegalStateException(e);
+		}
 		setCommands(commands);
 		setWorkingDirectory(Platform.getLocation().toOSString());
 	}
