fb4a9826a111df7d7e1049762ded1255def78ee5
Holger Schill <Holger.Schill@itemis.de>
2017-06-14 13:42:10 +0200

    Add vscode extension Start by calling ./gradlew startCode Server is self-contained in the extension

---
+++ org.xtext.calc.parent/vscode-calc-extension/src/extension.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import * as path from 'path';
+import * as os from 'os';
+
+import {Trace} from 'vscode-jsonrpc';
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/calc
+    let launcher = os.platform() === 'win32' ? 'calc-standalone.bat' : 'calc-standalone';
+    let script = context.asAbsolutePath(path.join('src', 'calc', 'bin', launcher));
+    
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        debug: { command: script, args: ['-Xdebug','-Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y','-Xmx256m'] }
+    };
+    
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['calc'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.trace = Trace.Verbose;
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+}
